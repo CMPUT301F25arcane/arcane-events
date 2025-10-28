87978d10846a85354e6ebd4c51ff7741f5f12804
AdityaAnoop3 <adityaanoop@live.in>
2025-10-28 17:51:42 -0600

    Profile Page Layout xml added

--- app/src/main/java/com/example/arcane/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/arcane/ui/notifications/NotificationsFragment.java
@@ -4,36 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.example.arcane.databinding.FragmentProfileBinding;
+import com.example.arcane.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
-    private FragmentProfileBinding binding;
+    private FragmentNotificationsBinding binding;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NotificationsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        
-        binding.editProfileButton.setOnClickListener(v -> {
-        });
-        
-        binding.toggleNotifications.setOnCheckedChangeListener((buttonView, isChecked) -> {
-        });
-        
-        binding.toggleGeolocation.setOnCheckedChangeListener((buttonView, isChecked) -> {
-        });
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
     }
 
     @Override
