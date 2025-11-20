d1c944685a5546884779a93201ac8e2821c85402
Devansh8321 <devansh.artwani123@gmail.com>
2025-11-19 18:05:33 -0700

    Add getters and setters to Notification

--- app/src/main/java/com/example/arcane/model/Notification.java
+++ app/src/main/java/com/example/arcane/model/Notification.java
@@ -63,5 +63,150 @@ public class Notification {
         this.timestamp = timestamp;
         this.read = read != null ? read : false;
     }
+
+    // Getters and Setters
+    /**
+     * Gets the notification document ID.
+     *
+     * @return the notification ID
+     */
+    public String getNotificationId() {
+        return notificationId;
+    }
+
+    /**
+     * Sets the notification document ID.
+     *
+     * @param notificationId the notification ID to set
+     */
+    public void setNotificationId(String notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    /**
+     * Gets the user ID.
+     *
+     * @return the user ID
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets the user ID.
+     *
+     * @param userId the user ID to set
+     */
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Gets the event ID.
+     *
+     * @return the event ID
+     */
+    public String getEventId() {
+        return eventId;
+    }
+
+    /**
+     * Sets the event ID.
+     *
+     * @param eventId the event ID to set
+     */
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    /**
+     * Gets the notification type.
+     *
+     * @return the type ("INVITED" or "LOST")
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the notification type.
+     *
+     * @param type the type to set ("INVITED" or "LOST")
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets the notification title.
+     *
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the notification title.
+     *
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Gets the notification message.
+     *
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Sets the notification message.
+     *
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Gets the notification timestamp.
+     *
+     * @return the timestamp
+     */
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Sets the notification timestamp.
+     *
+     * @param timestamp the timestamp to set
+     */
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * Gets whether the notification has been read.
+     *
+     * @return true if read, false otherwise
+     */
+    public Boolean getRead() {
+        return read;
+    }
+
+    /**
+     * Sets whether the notification has been read.
+     *
+     * @param read true if read, false otherwise
+     */
+    public void setRead(Boolean read) {
+        this.read = read;
+    }
 }
 
